b5b118c0d85f36138b4d119bd1d6fa9319884d1a
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2023-02-28 18:31:21 +0800

    E

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -72,14 +72,14 @@ export default function Header() {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <Link href="#" className="-m-1.5 p-1.5">
+            <span className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img
                 className="h-8 w-auto"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 alt=""
               />
-            </Link>
+            </span>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
