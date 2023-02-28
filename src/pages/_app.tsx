b9a7ec5ad283dfe04241ade6f4d42e4ac7b1c3f9
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2023-02-28 19:26:00 +0800

    E

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import Header from "~/components/Header";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -16,6 +17,17 @@ const MyApp: AppType<{ session: Session | null }> = ({
 
   return (
     <SessionProvider session={session}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="keywords" content="medical, tech" />
+        <meta
+          name="description"
+          content="Data to enrich your medical business"
+        />
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>Eldertechnovators</title>
+      </Head>
       {asPath !== "/" && <Header />}
       <Component {...pageProps} />
     </SessionProvider>
