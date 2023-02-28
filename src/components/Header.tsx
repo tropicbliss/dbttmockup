7b5d403493c9bdb1a49fa7df27a467fa03b87236
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2023-02-28 18:24:48 +0800

    E

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
       >
-        <Link href="#" className="-m-1.5 p-1.5">
+        <Link href="/" className="-m-1.5 p-1.5">
           <span className="sr-only">Your Company</span>
           <img
             className="h-8 w-auto"
