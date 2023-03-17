1fcdc68988ce1dea925f29167be76daca9d40058
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2023-03-17 16:53:39 +0800

    Add falling gif

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -10,7 +10,8 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import map from "../../public/map.png";
 import { Fragment, useState } from "react";
-import { Transition } from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
+import falling from "../../public/falling.gif";
 
 const cards = [
   { name: "Incidents resolved", icon: ScaleIcon, amount: "27" },
@@ -37,6 +38,7 @@ const statusStyles: { [key: string]: string } = {
 export default function Dashboard() {
   const { status } = useSession();
   const [noti, setNoti] = useState(false);
+  const [modal, setModal] = useState(false);
 
   if (status === "unauthenticated") {
     return (
@@ -118,6 +120,75 @@ export default function Dashboard() {
             </Transition>
           </div>
         </div>
+      </>
+      <>
+        <Transition.Root show={modal} as={Fragment}>
+          <Dialog as="div" className="relative z-10" onClose={setModal}>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 z-10 overflow-y-auto">
+              <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                  enterTo="opacity-100 translate-y-0 sm:scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                  leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                >
+                  <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                    <div className="absolute top-0 right-0 hidden pt-4 pr-4 sm:block">
+                      <button
+                        type="button"
+                        className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        onClick={() => setModal(false)}
+                      >
+                        <span className="sr-only">Close</span>
+                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                      </button>
+                    </div>
+                    <div className="sm:flex sm:items-start">
+                      <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                        <ExclamationTriangleIcon
+                          className="h-6 w-6 text-red-600"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <Dialog.Title
+                          as="h3"
+                          className="text-base font-semibold leading-6 text-gray-900"
+                        >
+                          Footage
+                        </Dialog.Title>
+                        <div className="mt-2">
+                          <Image
+                            className="rounded-md"
+                            src={falling}
+                            alt=""
+                            width={3000}
+                            height={1500}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </Dialog>
+        </Transition.Root>
       </>
       <div className="min-h-full">
         <div className="flex flex-1 flex-col">
@@ -337,6 +408,9 @@ export default function Dashboard() {
                               </td>
                               <td className="whitespace-nowrap px-6 py-4 text-right text-sm text-gray-500">
                                 <button
+                                  onClick={() => {
+                                    setModal(true);
+                                  }}
                                   type="button"
                                   className="rounded bg-indigo-600 py-1 px-2 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                 >
